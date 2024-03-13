3e88cfdd43833bd26ba93ee27900de7dce1b0975
rogallab <b-u.rogalla@ostfalia.de>
2024-03-13 13:42:51 +0100

    filePath as field

--- WebApp/Persistence/DataContextFake.cs
+++ WebApp/Persistence/DataContextFake.cs
@@ -7,37 +7,44 @@ using WebApp.DomainModel.Model;
 namespace WebApp.Persistence;
 
 public class DataContextFake : IDataContext {
-   
-   public Dictionary<Guid, Book> Books { get; } = new();
 
+   // File path  WIN: .AppData or Mac ./Library/Application Support
+   private readonly string _filePath = 
+      Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)
+      + "/WebApp03.json";
+   
+   // In-Memory Repository
+   public Dictionary<Guid, Book> Books { get; } = new();
+   
    public DataContextFake() {
-      // File path to read from
-      string pathAppData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
-      var filePath = pathAppData + "/WebApp03.json";
-      if (!File.Exists(filePath)) {
+      if (!File.Exists(_filePath)) {
          Books = new Dictionary<Guid, Book>();
          return;
       }
       else {
-         // Read JSON from file
-         string json = File.ReadAllText(filePath, Encoding.UTF8) ??
-            throw new ArgumentNullException("File.ReadAllText(filePath, Encoding.UTF8)");
-         Books = JsonSerializer.Deserialize<Dictionary<Guid, Book>>(json)
-            ?? throw new Exception("JsonSerializer.Deserialize is null)");
+         try {
+            // Read JSON from file
+            string json = File.ReadAllText(_filePath, Encoding.UTF8) ??
+               throw new ArgumentNullException("File.ReadAllText(filePath, Encoding.UTF8)");
+            // Deserialize JSON to Books
+            Books = JsonSerializer.Deserialize<Dictionary<Guid, Book>>(json)
+               ?? throw new Exception("JsonSerializer.Deserialize is null)");
+         }
+         catch (Exception e) {
+            Console.WriteLine(e.Message);
+         }
       }
    }
 
    public bool SaveAllChanges() {
       try {
+         // Serialize Books to JSON
          string json = JsonSerializer.Serialize(
             Books,
             new JsonSerializerOptions { WriteIndented = true }
          );
-         // File path to write to
-         string pathAppData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
-         var filePath = pathAppData + "/WebApp03.json";
          // Write JSON string to file
-         File.WriteAllText(filePath, json, Encoding.UTF8);
+         File.WriteAllText(_filePath, json, Encoding.UTF8);
          return true;
       }
       catch (Exception e) {
